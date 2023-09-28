99ac4baefade585432fe659fdb3f716cc00d3ecf
Sarah Habib <habibsarah11@gmail.com>
2023-09-27 17:04:30 -0700

    Add LocalTetrad to PointwiseFunctions

---
+++ src/PointwiseFunctions/GeneralRelativity/LocalTetrad.hpp
@@ -0,0 +1,42 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/Tensor/EagerMath/DeterminantAndInverse.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "DataStructures/VariablesTag.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/ContainerHelpers.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+/// \ingroup GeneralRelativityGroup
+/// Holds functions related to general relativity.
+namespace gr {
+/// @{
+/*!
+ * \ingroup GeneralRelativityGroup
+ * \brief Compute transformation to local tetrad from lapse, shift, and spatial metric
+ *
+ * \details
+ * 
+ */
+
+template <typename DataType, size_t SpatialDim>
+void local_tetrad(gsl::not_null<tnsr::Ab<DataType, SpatialDim, Frame>*> local_tetrad_tensor,
+           gsl::not_null<tnsr::Ab<DataType, SpatialDim, Frame>*> inverse_local_tetrad_tensor,
+           const Scalar<DataType>& lapse,
+           const tnsr::I<DataType, SpatialDim, Frame>& shift,
+           const tnsr::ii<DataType, SpatialDim, Frame>& spatial_metric,
+           const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric);
+/// @}
+}  // namespace gr
