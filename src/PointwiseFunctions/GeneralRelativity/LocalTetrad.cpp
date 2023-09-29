7c9f32a4484c76764b10756e58ab77845b4701da
Sarah Habib <habibsarah11@gmail.com>
2023-09-29 02:10:01 -0700

    Complete tetrad computation

--- src/PointwiseFunctions/GeneralRelativity/LocalTetrad.cpp
+++ src/PointwiseFunctions/GeneralRelativity/LocalTetrad.cpp
@@ -14,19 +14,18 @@
 
 namespace gr {
 
-template <typename DataType, size_t SpatialDim>
+template <typename DataType>
 void local_tetrad(
-    gsl::not_null<tnsr::Ab<DataType, SpatialDim, Frame>*> local_tetrad_tensor,
-    gsl::not_null<tnsr::Ab<DataType, SpatialDim, Frame>*>
-        inverse_local_tetrad_tensor,
+    gsl::not_null<tnsr::Ab<DataType, 3>*> local_tetrad_tensor,
+    gsl::not_null<tnsr::Ab<DataType, 3>*> inverse_local_tetrad_tensor,
     const Scalar<DataType>& lapse,
-    const tnsr::I<DataType, SpatialDim, Frame>& shift,
-    const tnsr::ii<DataType, SpatialDim, Frame>& spatial_metric,
-    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric) {
+    const tnsr::I<DataType, 3, Frame::Inertial>& shift,
+    const tnsr::ii<DataType, 3, Frame::Inertial>& spatial_metric,
+    const tnsr::II<DataType, 3, Frame::Inertial>& inverse_spatial_metric) {
   // Define helper variables
-  const auto Z = 1 / square(get(lapse));
+  const auto Z = 1.0 / square(get(lapse));
   const auto B = square(get(lapse)) / sqrt(get<0, 0>(inverse_spatial_metric));
-  const auto C = 1 / sqrt(get<2, 2>(spatial_metric));
+  const auto C = 1.0 / sqrt(get<2, 2>(spatial_metric));
   const auto D =
       C / sqrt(get<1, 1>(spatial_metric) * get<2, 2>(spatial_metric) -
                square(get<1, 2>(spatial_metric)));
@@ -38,9 +37,42 @@ void local_tetrad(
   const auto H = Z * get<0, 2>(inverse_spatial_metric);
 
   // Compute transformation components
-  get<0, 0>(*local_tetrad_tensor) = 1 / get(lapse);
+  get<0, 0>(*local_tetrad_tensor) = 1.0 / get(lapse);
   get<1, 0>(*local_tetrad_tensor) = -get<0>(shift) / get(lapse);
   get<1, 1>(*local_tetrad_tensor) = sqrt(get<0, 0>(inverse_spatial_metric));
+  get<2, 0>(*local_tetrad_tensor) = -get<1>(shift) / get(lapse);
+  get<2, 1>(*local_tetrad_tensor) = get<0, 0>(inverse_spatial_metric) /
+                                    sqrt(get<0, 1>(inverse_spatial_metric));
+  get<2, 2>(*local_tetrad_tensor) = D * get<2, 2>(spatial_metric);
+  get<3, 0>(*local_tetrad_tensor) = -get<2>(shift) / get(lapse);
+  get<3, 1>(*local_tetrad_tensor) = get<0, 0>(inverse_spatial_metric) /
+                                    sqrt(get<0, 2>(inverse_spatial_metric));
+  get<3, 2>(*local_tetrad_tensor) = D * get<1, 2>(spatial_metric);
+  get<3, 3>(*local_tetrad_tensor) = C;
+
+  // Compute inverse components
+  get<0, 0>(*inverse_local_tetrad_tensor) = get(lapse);
+  get<1, 0>(*inverse_local_tetrad_tensor) =
+      get<0>(shift) / sqrt(get<0, 0>(inverse_spatial_metric));
+  get<1, 1>(*inverse_local_tetrad_tensor) =
+      1.0 / sqrt(get<0, 0>(inverse_spatial_metric));
+  get<2, 0>(*inverse_local_tetrad_tensor) =
+      -square(B) * E / (D * get<2, 2>(spatial_metric) * square(get(lapse)));
+  get<2, 1>(*inverse_local_tetrad_tensor) =
+      -square(B) * F / (D * get<2, 2>(spatial_metric) * square(get(lapse)));
+  get<2, 2>(*inverse_local_tetrad_tensor) = 1 / (D * get<2, 2>(spatial_metric));
+  get<3, 0>(*inverse_local_tetrad_tensor) =
+      -(square(B) / C) *
+      (G + E * get<1, 2>(spatial_metric) / get<2, 2>(spatial_metric)) /
+      square(get(lapse));
+  get<3, 1>(*inverse_local_tetrad_tensor) =
+      -(square(B) / C) *
+      (H + F * get<1, 2>(spatial_metric) / get<2, 2>(spatial_metric)) /
+      square(get(lapse));
+  ;
+  get<3, 2>(*inverse_local_tetrad_tensor) = get<1, 2>(spatial_metric) /
+                                            (C * get<2, 2>(spatial_metric));
+  get<3, 3>(*inverse_local_tetrad_tensor) = 1.0 / C;
 }
 }  // namespace gr
 
@@ -48,14 +80,18 @@ void local_tetrad(
 #define FRAME(data) BOOST_PP_TUPLE_ELEM(1, data)
 
 // TODO: Fix this...
-#define INSTANTIATE(_, data)                             \
-  template void gr::local_tetrad(                        \
-      const gsl::not_null<Scalar<DTYPE(data)>*> lapse,   \
-      const tnsr::I<DTYPE(data), 3, FRAME(data)>& shift, \
+#define INSTANTIATE(_, data)                                                  \
+  template void gr::local_tetrad(                                             \
+      const gsl::not_null<tnsr::Ab<DTYPE(data), 3>*>                          \
+      local_tetrad_tensor,                                                    \
+      const gsl::not_null<tnsr::Ab<DTYPE(data), 3>*>                          \
+      inverse_local_tetrad_tensor,                                            \
+      const Scalar<DTYPE(data)> lapse,                                        \
+      const tnsr::I<DTYPE(data), 3, FRAME(data)>& shift,                      \
       const tnsr::aa<DTYPE(data), 3, FRAME(data)>& spacetime_metric);
 
 GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3), (double, DataVector),
-                        (Frame::Grid, Frame::Inertial))
+                      FRAME(data))
 
 #undef DIM
 #undef DTYPE
--- src/PointwiseFunctions/GeneralRelativity/LocalTetrad.hpp
+++ src/PointwiseFunctions/GeneralRelativity/LocalTetrad.hpp
@@ -25,18 +25,21 @@ namespace gr {
 /// @{
 /*!
  * \ingroup GeneralRelativityGroup
- * \brief Compute transformation to local tetrad from lapse, shift, and spatial metric
+ * \brief Compute transformation to local tetrad from lapse, shift, and spatial
+ * metric
  *
  * \details
- * 
+ *
  */
 
-template <typename DataType, size_t SpatialDim>
-void local_tetrad(gsl::not_null<tnsr::Ab<DataType, SpatialDim, Frame>*> local_tetrad_tensor,
-           gsl::not_null<tnsr::Ab<DataType, SpatialDim, Frame>*> inverse_local_tetrad_tensor,
-           const Scalar<DataType>& lapse,
-           const tnsr::I<DataType, SpatialDim, Frame>& shift,
-           const tnsr::ii<DataType, SpatialDim, Frame>& spatial_metric,
-           const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric);
+template <typename DataType>
+void local_tetrad(
+    gsl::not_null<tnsr::Ab<DataType, 3, Frame::Inertial>*> local_tetrad_tensor,
+    gsl::not_null<tnsr::Ab<DataType, 3, Frame::Inertial>*>
+        inverse_local_tetrad_tensor,
+    const Scalar<DataType>& lapse,
+    const tnsr::I<DataType, 3, Frame::Inertial>& shift,
+    const tnsr::ii<DataType, 3, Frame::Inertial>& spatial_metric,
+    const tnsr::II<DataType, 3, Frame::Inertial>& inverse_spatial_metric);
 /// @}
 }  // namespace gr
