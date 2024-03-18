f43329fd6dbe21b9721656af6212c2cc9bdd813c
Sarah Habib <habibsarah11@gmail.com>
2024-03-18 16:37:29 -0700

    Add hybridization parameter to Gforce

--- src/Evolution/Systems/GrMhd/ValenciaDivClean/BoundaryCorrections/Gforce.cpp
+++ src/Evolution/Systems/GrMhd/ValenciaDivClean/BoundaryCorrections/Gforce.cpp
@@ -169,6 +169,8 @@ void Gforce::dg_boundary_terms(
   tnsr::I<DataVector, 3, Frame::Inertial> tilde_b_LW{};
   Scalar<DataVector> tilde_phi_LW{};
 
+  const auto omega_g = 0.5;
+
   get(tilde_d_LW) =
       0.5 * (get(tilde_d_ext) + get(tilde_d_int)) -
       0.5 * (1.0 / max(get(abs_char_speed_int), get(abs_char_speed_ext))) *
@@ -326,83 +328,83 @@ void Gforce::dg_boundary_terms(
 
   if (dg_formulation == dg::Formulation::WeakInertial) {
     get(*boundary_correction_tilde_d) =
-        0.5 * (0.5 * (get(normal_dot_flux_tilde_d_int) -
+        (1.0 - omega_g) * (0.5 * (get(normal_dot_flux_tilde_d_int) -
                       get(normal_dot_flux_tilde_d_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_d_ext) - get(tilde_d_int))) +
-        0.5 * get(normal_dot_flux_tilde_d_LW);
+        omega_g * get(normal_dot_flux_tilde_d_LW);
     get(*boundary_correction_tilde_ye) =
-        0.5 * (0.5 * (get(normal_dot_flux_tilde_ye_int) -
+        (1.0 - omega_g) * (0.5 * (get(normal_dot_flux_tilde_ye_int) -
                       get(normal_dot_flux_tilde_ye_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_ye_ext) - get(tilde_ye_int))) +
-        0.5 * get(normal_dot_flux_tilde_ye_LW);
+        omega_g * get(normal_dot_flux_tilde_ye_LW);
     get(*boundary_correction_tilde_tau) =
-        0.5 * (0.5 * (get(normal_dot_flux_tilde_tau_int) -
+        (1.0 - omega_g) * (0.5 * (get(normal_dot_flux_tilde_tau_int) -
                       get(normal_dot_flux_tilde_tau_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_tau_ext) - get(tilde_tau_int))) +
-        0.5 * get(normal_dot_flux_tilde_tau_LW);
+        omega_g * get(normal_dot_flux_tilde_tau_LW);
     get(*boundary_correction_tilde_phi) =
-        0.5 * (0.5 * (get(normal_dot_flux_tilde_phi_int) -
+        (1.0 - omega_g) * (0.5 * (get(normal_dot_flux_tilde_phi_int) -
                       get(normal_dot_flux_tilde_phi_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_phi_ext) - get(tilde_phi_int))) +
-        0.5 * get(normal_dot_flux_tilde_phi_LW);
+        omega_g * get(normal_dot_flux_tilde_phi_LW);
 
     for (size_t i = 0; i < 3; ++i) {
       boundary_correction_tilde_s->get(i) =
-          0.5 * (0.5 * (normal_dot_flux_tilde_s_int.get(i) -
+          (1.0 - omega_g) * (0.5 * (normal_dot_flux_tilde_s_int.get(i) -
                         normal_dot_flux_tilde_s_ext.get(i)) -
                  0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                      (tilde_s_ext.get(i) - tilde_s_int.get(i))) +
-          0.5 * normal_dot_flux_tilde_s_LW.get(i);
+          omega_g * normal_dot_flux_tilde_s_LW.get(i);
       boundary_correction_tilde_b->get(i) =
-          0.5 * (0.5 * (normal_dot_flux_tilde_b_int.get(i) -
+          (1.0 - omega_g) * (0.5 * (normal_dot_flux_tilde_b_int.get(i) -
                         normal_dot_flux_tilde_b_ext.get(i)) -
                  0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                      (tilde_b_ext.get(i) - tilde_b_int.get(i))) +
-          0.5 * normal_dot_flux_tilde_b_LW.get(i);
+          omega_g * normal_dot_flux_tilde_b_LW.get(i);
     }
   } else {
     get(*boundary_correction_tilde_d) =
-        0.5 * (-0.5 * (get(normal_dot_flux_tilde_d_int) +
+        (1.0 - omega_g) * (-0.5 * (get(normal_dot_flux_tilde_d_int) +
                        get(normal_dot_flux_tilde_d_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_d_ext) - get(tilde_d_int))) +
-        0.5 * get(normal_dot_flux_tilde_d_LW);
+        omega_g * get(normal_dot_flux_tilde_d_LW);
     get(*boundary_correction_tilde_ye) =
-        0.5 * (-0.5 * (get(normal_dot_flux_tilde_ye_int) +
+        (1.0 - omega_g) * (-0.5 * (get(normal_dot_flux_tilde_ye_int) +
                        get(normal_dot_flux_tilde_ye_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_ye_ext) - get(tilde_ye_int))) +
-        0.5 * get(normal_dot_flux_tilde_ye_LW);
+        omega_g * get(normal_dot_flux_tilde_ye_LW);
     get(*boundary_correction_tilde_tau) =
-        0.5 * (-0.5 * (get(normal_dot_flux_tilde_tau_int) +
+        (1.0 - omega_g) * (-0.5 * (get(normal_dot_flux_tilde_tau_int) +
                        get(normal_dot_flux_tilde_tau_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_tau_ext) - get(tilde_tau_int))) +
-        0.5 * get(normal_dot_flux_tilde_tau_LW);
+        omega_g * get(normal_dot_flux_tilde_tau_LW);
     get(*boundary_correction_tilde_phi) =
-        0.5 * (-0.5 * (get(normal_dot_flux_tilde_phi_int) +
+        (1.0 - omega_g) * (-0.5 * (get(normal_dot_flux_tilde_phi_int) +
                        get(normal_dot_flux_tilde_phi_ext)) -
                0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                    (get(tilde_phi_ext) - get(tilde_phi_int))) +
-        0.5 * get(normal_dot_flux_tilde_phi_LW);
+        omega_g * get(normal_dot_flux_tilde_phi_LW);
 
     for (size_t i = 0; i < 3; ++i) {
       boundary_correction_tilde_s->get(i) =
-          0.5 * (-0.5 * (normal_dot_flux_tilde_s_int.get(i) +
+          (1.0 - omega_g) * (-0.5 * (normal_dot_flux_tilde_s_int.get(i) +
                          normal_dot_flux_tilde_s_ext.get(i)) -
                  0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                      (tilde_s_ext.get(i) - tilde_s_int.get(i))) +
-          0.5 * normal_dot_flux_tilde_s_LW.get(i);
+          omega_g * normal_dot_flux_tilde_s_LW.get(i);
       boundary_correction_tilde_b->get(i) =
-          0.5 * (-0.5 * (normal_dot_flux_tilde_b_int.get(i) +
+          (1.0 - omega_g) * (-0.5 * (normal_dot_flux_tilde_b_int.get(i) +
                          normal_dot_flux_tilde_b_ext.get(i)) -
                  0.5 * max(get(abs_char_speed_int), get(abs_char_speed_ext)) *
                      (tilde_b_ext.get(i) - tilde_b_int.get(i))) +
-          0.5 * normal_dot_flux_tilde_b_LW.get(i);
+          omega_g * normal_dot_flux_tilde_b_LW.get(i);
     }
   }
 }
